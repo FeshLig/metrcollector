14967a277ae41962e2746cd2c11e59aa2dd8ae0e
Feshlig <www.artempir762003@gmail.com>
2025-12-28 13:03:32 +0300

    comleted iter2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"time"
+
+	"github.com/FeshLig/metrcollector/internal/agent"
+	"github.com/FeshLig/metrcollector/internal/repository"
+)
+
+func main() {
+	pollInterval := time.Duration(2)
+	reportInterval := time.Duration(10)
+
+	storage := repository.NewMemStorage()
+	client := &http.Client{}
+
+	collector := agent.NewMetricCollector(storage)
+	sender := agent.NewSender("http://localhost:8080", client)
+
+	go func() {
+		for {
+			collector.CollectMetrics()
+			time.Sleep(pollInterval * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			sender.SendMetrics(storage)
+			time.Sleep(reportInterval * time.Second)
+		}
+	}()
+
+	select {}
+
+}
